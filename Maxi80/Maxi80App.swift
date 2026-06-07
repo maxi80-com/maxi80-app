7b57794a47c14f8de17ca43279020023ec5ef31d
Sebastien Stormacq <sebastien.stormacq@gmail.com>
2026-06-07 13:52:59 +0200

    Initial Commit

---
+++ Maxi80/Maxi80App.swift
@@ -0,0 +1,17 @@
+//
+//  Maxi80App.swift
+//  Maxi80
+//
+//  Created by Sebastien Stormacq on 07/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct Maxi80App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Maxi80Tests/Maxi80Tests.swift
@@ -0,0 +1,18 @@
+//
+//  Maxi80Tests.swift
+//  Maxi80Tests
+//
+//  Created by Sebastien Stormacq on 07/06/2026.
+//
+
+import Testing
+
+struct Maxi80Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
